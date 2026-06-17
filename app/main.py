d3a9690b1d2030accb47267979decc9b6ecdae7b
hami <hamop3108@gmail.com.com>
2026-06-17 11:59:50 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,11 +4,18 @@ import sys
 def main():
     while True:
         sys.stdout.write("$ ")
+
         command = input()
+
+        # exit
         if command == "exit":
             break
-        elif command.startswith("echo "):
+        
+        # echo
+        if command.startswith("echo "):
             print(command[5:])
+        else:
+            print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
