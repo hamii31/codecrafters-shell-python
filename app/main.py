d36cc87f5a4ff7fb18ddca97e53be4ca4d28009c
hami <hamop3108@gmail.com.com>
2026-07-20 09:00:30 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -51,7 +51,7 @@ def main():
                         continue
             # cd
             if command.startswith("cd "):
-                path = command.split(" ")
+                path = command.split("/")
                 path = ''.join(path)
                 if os.path.exists(path):
                     os.chdir(path)
