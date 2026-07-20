fa36b915a7c054ae398ecb8612cd126786ea6577
hami <hamop3108@gmail.com.com>
2026-07-20 09:53:40 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -71,7 +71,8 @@ def main():
             if command.startswith("echo "):
                 output = command[5:]
                 if output.startswith("'") and output.endswith("'") and output.count("'") == 2:
-                    print(output.replace("'", ""))
+                    output = output.replace("'", "")
+                    print(output)
                 elif output.startswith("'") and output.endswith("'") and output.count("'") != 2:
                     print(output.replace(" ", ""))
                 elif not output.startswith("'") and not output.startswith("'") and output.count("'") != 0:
