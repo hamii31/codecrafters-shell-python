f4742cab4d1dbc1ec0c0ab5abbb527092583a50e
hami <hamop3108@gmail.com.com>
2026-07-28 13:38:12 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ def completer(text, state):
             for name in entries:
                 if name.startswith(text):
                     full_path = os.path.join(directory, name)
-                    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                    if os.path.isfile(full_path) and os.access(full_path, os.F_OK):
                         matches.append(name + " ")
 
     if state < len(matches):
